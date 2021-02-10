cbed1fe983b4c96e961581987cb7228ca12aa1fe
Drake <apple@apples-MacBook-Pro.local>
2021-02-10 11:08:39 -0500

    begin

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
+let listName = document.querySelectorAll(".listname");
+let hoverList = document.querySelectorAll(".hoverlist");
+let hoverListItems = document.querySelectorAll(".listItem");
+let ae = document.querySelector(".A");
+let be = document.querySelector(".B");
+let ce = document.querySelector(".C");
+let de = document.querySelector(".D");
+let ee = document.querySelector(".E");
+listName.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    if (item.innerText === "Mind") {
+      ae.style.display = "block";
+      opac();
+    } else if (item.innerText === "Body") {
+      be.style.display = "block";
+      console.log(hoverListItems);
+    }
+  });
+});
 
+// hoverList.style.display = "block";
+// hoverListItems.style.opacity = "1";
+let opac = function () {
+  hoverListItems.forEach((item) => {
+    item.classList.add("opac");
+  });
+};
+let opac2 = function () {
+  hoverListItems.forEach((item) => {
+    item.classList.delete("opac");
+  });
+};
